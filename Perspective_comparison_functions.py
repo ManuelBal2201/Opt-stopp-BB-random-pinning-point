84f269f534edfb0fc1e4593176405f4d889cefa6
ManuelBal2201 <manuelbal2201@gmail.com>
2025-05-24 16:55:12 +0200

    Fulfill documentation

--- Perspective_comparison_functions.py
+++ Perspective_comparison_functions.py
@@ -1,13 +1,27 @@
 # Libraries
 import numpy as np
-from scipy.stats import rv_discrete, rv_continuous, norm
-from scipy.integrate import quad
-from scipy.interpolate import interp1d
-import random
-
 
 # Brownian bridges simulation
 def simulate_brownian_bridge(t, z_t, T, z_T, u=None, n_steps=1000):
+    """
+
+    Simulates a Brownian bridge process between two fixed points.
+
+    Parameters:
+    - t (float): Starting time of the process. Must satisfy 0 <= t < T.
+    - z_t (float): Position of the process at time t.
+    - T (float): Ending time of the process.
+    - z_T (float): Position of the process at time T.
+    - u (float): If provided, the step size for the point of interest. Must satisfy t + u <= T.
+    - n_steps (int): Number of steps for full path simulation (used only if u is None).
+
+    Returns:
+    - next_x (float): If `u` is provided, returns the next value of the Brownian bridge at time t + u.
+    - all_times (np.array): If `u` is None, returns an array of time points between t and T.
+    - path (np.array): If `u` is None, returns the full simulated Brownian bridge path.
+
+    """
+    
     assert 0 <= t < T, "t must be in [0, T)"
     if u is not None:
       assert t < t+u <= T, "t + u must be in (t, T]"
